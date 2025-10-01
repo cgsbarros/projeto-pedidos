bce13b6c76b4b569cd1913a36707129d11252aaf
cgsbarros <caua.barros@tcema.tc.br>
2025-10-01 08:58:45 -0300

    feat: Tratamento de Exception findById

--- src/main/java/com/cauabarros/aulajpacurso/services/UserService.java
+++ src/main/java/com/cauabarros/aulajpacurso/services/UserService.java
@@ -2,6 +2,7 @@ package com.cauabarros.aulajpacurso.services;
 
 import com.cauabarros.aulajpacurso.entities.User;
 import com.cauabarros.aulajpacurso.repositories.UserRepository;
+import com.cauabarros.aulajpacurso.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -17,7 +18,7 @@ public class UserService {
     }
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
     public User insert(User obj){
         return repository.save(obj);
